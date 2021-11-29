d6463f9294f6e91f9a227fa9ecfac1236812d445
Taty <tatyanaparks@gmail.com>
2021-11-29 14:47:59 -0600

    made new components

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -2,10 +2,24 @@ import React from 'react'
 
 const PostCard = () => {
     return (
-        <div>
-            
+      // <div key={post.id} className="card my-3"> this is what it will look like once we can pass the props
+      <div className="card my-3">
+        <div className="card-header">
+          <Link to="/">
+            Post title goes here, link goes to the id of a specific post
+          </Link>
         </div>
-    )
+        <div className="card-body">
+          <blockquote className="blockquote mb-0">
+            <p>post content here</p>
+            <footer className="blockquote-footer">
+              Author goes here
+              <cite title="Source Title">Date created goes here</cite>
+            </footer>
+          </blockquote>
+        </div>
+      </div>
+    );
 }
 
 export default PostCard
--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -2,10 +2,20 @@ import React from 'react'
 
 const PostDetail = () => {
     return (
-        <div>
-            
+      <div>
+        <div className="card my-3">
+          <div className="card-header">Title of post here</div>
+          <div className="card-body">
+            <blockquote className="blockquote mb-0">
+              <p>post content here</p>
+              <footer className="blockquote-footer">
+                <cite title="Source Title">date created here</cite>
+              </footer>
+            </blockquote>
+          </div>
         </div>
-    )
+      </div>
+    );
 }
 
 export default PostDetail
